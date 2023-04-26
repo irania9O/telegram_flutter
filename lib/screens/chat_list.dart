9c2f8b1b3a8c5a3d79827336ba120ff7f431dda6
Seyed Ali <wwwwwq37@gmail.com>
2023-04-26 22:12:54 +0330

    update chat_list page.

--- lib/screens/chat_list.dart
+++ lib/screens/chat_list.dart
@@ -10,26 +10,45 @@ class ChatListPage extends StatelessWidget {
     return ListView.separated(
         itemCount: count,
         itemBuilder: (context, index) {
-          return const Padding(
-              padding: EdgeInsets.symmetric(vertical: 5),
+          return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2),
               child: ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://avatars.githubusercontent.com/u/33866360?v=4'),
-                ),
-                // truncateMode: TruncateMode.middle,
-                title: Text(
-                  'Seyed Ali Kamali Pargoo',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                subtitle: Text(
-                  'Hello World!',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: Text('18:00'),
-              ));
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      // 'https://avatars.githubusercontent.com/u/33866360?v=4'
+                      'https://i.pravatar.cc/100?img=$index',
+                    ),
+                  ),
+                  // truncateMode: TruncateMode.middle,
+                  title: const Text(
+                    'Seyed Ali Kamali Pargoo',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: const Text(
+                    'Hello World!',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  trailing: Column(
+                    children: [
+                      const Text('01:01'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CircleAvatar(
+                        radius: 11,
+                        backgroundColor: Colors.green[700],
+                        child: Text(
+                          '${index + 1}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )));
         },
         separatorBuilder: (context, index) {
           return const Divider(
