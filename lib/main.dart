3b0c233479b7fb32a153815e1864ddfc2c244997
Seyed Ali <wwwwwq37@gmail.com>
2023-04-26 14:26:07 +0330

    add floatingActionButton, drawer.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       darkTheme: ThemeData.dark(),
       theme: ThemeData.light(),
-      themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      themeMode: ThemeMode.light,
+      // themeMode: ThemeMode.system,
+      home: const MyHomePage(title: 'Telegram'),
     );
   }
 }
@@ -33,10 +38,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+          child: ListView(
+        children: const [
+          UserAccountsDrawerHeader(
+            accountName: Text('Flutter'),
+            accountEmail: Text('AS'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/19484515?v=4'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/180930?v=4'),
+                  fit: BoxFit.cover),
+            ),
+            otherAccountsPictures: [
+              // On press of this icon you can navigate to another page
+              BackButton(
+                // onPressed: () {},
+                color: Colors.white,
+              )
+            ],
+          ),
+          // ListTile(
+          //   leading: Icon(Icons.message),
+          //   title: Text('Messages'),
+          // ),
+        ],
+      )),
       body: const Center(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: const Icon(Icons.add),
+        child: Icon(PhosphorIcons.fill.pencil),
       ),
     );
   }
